c6200a7eccf7eefdb6fb71ac6e6549f6b4835d1a
Denis <ddujota@internationalsafety.com>
2018-11-09 01:10:24 -0500

    fixed spacing on comments

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const path = require('path');
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-//API Routes
+// API Routes
 router.use('/api', apiRoutes);
 
-// If no API routes are hit, send the react app
-
+// If no API routes are hit, send the React app
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
